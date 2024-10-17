e65576402dcbf20ce88196088498804263e90358
Ana Karol <151869218+anakarolcatu@users.noreply.github.com>
2024-10-17 19:11:29 -0300

    add cadastrar veiculo

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -16,7 +16,11 @@ namespace DesafioFundamentos.Models
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
-            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            Console.WriteLine("Digite a placa do veículo a ser adicionado:");
+            string placa = Console.ReadLine();
+            veiculos.Add(placa);
+            Console.WriteLine("Veículo cadastrado com sucesso!") 
+            
         }
 
         public void RemoverVeiculo()
